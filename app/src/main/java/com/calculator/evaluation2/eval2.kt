fb1892bf6a1519f5c1d1dd549f32bf7027e4c7dd
Akshay <akshay.savanoor@codecraft.co.in>
2023-08-18 16:02:05 +0530

    Implemented otp verification and success page

---
+++ app/src/main/java/com/calculator/evaluation2/eval2.kt
@@ -0,0 +1,11 @@
+package com.calculator.evaluation2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class eval2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eval2)
+    }
+}
---
+++ app/src/main/java/com/calculator/evaluation2/validation2.kt
@@ -0,0 +1,11 @@
+package com.calculator.evaluation2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class validation2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_validation2)
+    }
+}
